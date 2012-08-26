9e656c30242d3bd2f6ad2c93d15ae38b828168ba
teenst <teens.twenties@gmail.com>
2012-08-26 16:08:38 +0900

    exe15

---
+++ exe15.rb
@@ -0,0 +1,32 @@
+#!/usr/env/bin ruby
+# -*- coding: utf-8 -*-
+
+require "pp"
+
+name = []
+while line = gets
+  if line.index(/^TEXT:/)
+    name += line.scan(/(@\w+)/)
+  end
+end
+usernames = name.flatten!.uniq!
+
+puts "<html><body>"
+
+usernames.each do |id|
+  id.delete!("@")
+  puts "<a href=\"http://twitter.com/#!/#{id}\">#{id}</a><br>"
+end
+puts "</body></html>"
+
+
+
+
+
+
+
+
+
+
+
+
